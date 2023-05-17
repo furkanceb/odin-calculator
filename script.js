2eb968a4bef981d4f6b59163fe722a52cce9f8ad
furkanceb <117816558+furkanceb@users.noreply.github.com>
2023-05-17 17:50:46 +0300

    Create function operate

--- script.js
+++ script.js
@@ -2,23 +2,27 @@ let firstNumber;
 let secondNumber;
 let operator;
 
-function add(a,b){
-    return a+b;
+function add(num1,num2){
+    return num1+num2;
 }
 
-function substract(a,b){
-    return a-b;
+function substract(num1,num2){
+    return num1-num2;
 }
 
-function multiply(a,b){
-    return a*b;    
+function multiply(num1,num2){
+    return num1*num2;    
 }
 
-function divide(a,b){
-    return a/b;
+function divide(num1,num2){
+    return num1/num2;
 }
 
+function operate(num1,num2,operater){
+    return operater(num1,num2);
+}
 console.log(add(3,6));
 console.log(substract(9,3));
 console.log(multiply(5,2));
 console.log(divide(8,2));
+console.log(operate(6,3,divide));
