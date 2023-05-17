5cc4c93ae00f002208eaba006c39332b472ee4a0
furkanceb <117816558+furkanceb@users.noreply.github.com>
2023-05-17 17:42:01 +0300

    Create basic math functions

---
+++ script.js
@@ -0,0 +1,24 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+function add(a,b){
+    return a+b;
+}
+
+function substract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;    
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+console.log(add(3,6));
+console.log(substract(9,3));
+console.log(multiply(5,2));
+console.log(divide(8,2));
