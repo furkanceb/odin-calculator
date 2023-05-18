3add1ab6029d71a5b0dc93cd394267ce776f23b7
furkanceb <117816558+furkanceb@users.noreply.github.com>
2023-05-18 15:02:44 +0300

    Add functionality to operator buttons

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-let firstNumber;
-let secondNumber;
+let firstNumber=5;
+let secondNumber=3;
 let operator;
 
-function add(num1,num2){
-    return num1+num2;
+function add(firstNumber,secondNumber){
+    return firstNumber+secondNumber;
 }
 
-function substract(num1,num2){
-    return num1-num2;
+function substract(firstNumber,secondNumber){
+    return firstNumber-secondNumber;
 }
 
-function multiply(num1,num2){
-    return num1*num2;    
+function multiply(firstNumber,secondNumber){
+    return firstNumber*secondNumber;    
 }
 
-function divide(num1,num2){
-    return num1/num2;
+function divide(firstNumber,secondNumber){
+    return firstNumber/secondNumber;
 }
 
-function operate(num1,num2,operater){
-    return operater(num1,num2);
+function operate(firstNumber,secondNumber,operator){
+    return operator(firstNumber,secondNumber);
 }
 console.log(add(3,6));
 console.log(substract(9,3));
@@ -28,6 +28,7 @@ console.log(divide(8,2));
 console.log(operate(6,3,divide));
 
 const screen=document.querySelector(".screen-child");
+
 let numberElement=document.querySelectorAll(".number");
 numberElement.forEach(element=>{
     element.addEventListener('click',(event) => {
@@ -36,4 +37,29 @@ numberElement.forEach(element=>{
     number.textContent=element.textContent;  
         screen.appendChild(number);
         console.log(element.textContent);});
+});
+
+let addButton=document.querySelector(".add");
+addButton.addEventListener('click',(evevnt)=>{
+    operator=add;
+});
+
+let substractButton=document.querySelector(".substract");
+substractButton.addEventListener('click',(evevnt)=>{
+    operator=substract;
+});
+
+let multiplyButton=document.querySelector(".multiply");
+multiplyButton.addEventListener('click',(evevnt)=>{
+    operator=multiply;
+});
+
+let divideButton=document.querySelector(".divide");
+divideButton.addEventListener('click',(evevnt)=>{
+    operator=divide;
+});
+
+let operateButton=document.querySelector(".operate");
+operateButton.addEventListener('click',(evevnt)=>{
+    console.log(operate(firstNumber,secondNumber,operator));
 });
